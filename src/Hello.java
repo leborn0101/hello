906a483cf61c518cace8af47fdc1c033b89cd286
leborn <leborn@yeah.net>
2017-12-07 21:49:49 +0800

    add sentence 'hello world too'

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("hello world!");
         System.out.println("world");
         System.out.println("hello");
+        System.out.println("hello world too");
     }
 }
